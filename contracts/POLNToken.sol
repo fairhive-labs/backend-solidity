6ef13ef9d9abbb469119b420120fb5ee626e046e
_whyvrafvr <jsie@trendev.fr>
2022-05-13 18:19:49 +0200

    init token contract

---
+++ contracts/POLNToken.sol
@@ -0,0 +1,11 @@
+// contracts/POLNToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract POLNToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("fairhive token", "POLN") {
+        _mint(msg.sender, initialSupply);
+    }
+}
