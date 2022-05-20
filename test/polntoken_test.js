3748f98c6285b11207b5057ae04373805968b57b
_whyvrafvr <jsie@trendev.fr>
2022-05-21 01:14:32 +0200

    add more asserts + improve transfer test

--- test/polntoken_test.js
+++ test/polntoken_test.js
@@ -1,6 +1,8 @@
 const POLNToken = artifacts.require("POLNToken");
 
 contract("POLNToken", (accounts) => {
+    const totalSupply = web3.utils.toBN(web3.utils.toWei("200000000", "ether"));
+
     it("is deployed", async () => {
         const contract = await POLNToken.deployed();
         assert(contract, "contract is not deployed");
@@ -9,15 +11,15 @@ contract("POLNToken", (accounts) => {
 
     it("has expected decimals", async () => {
         const contract = await POLNToken.deployed();
-        const d = await contract.decimals();
-        const decimals = web3.utils.toBN(d);
-        assert(web3.utils.toBN(18).eq(decimals));
+        const decimals = web3.utils.toBN(await contract.decimals());
+        assert(decimals);
+        assert(decimals.eq(web3.utils.toBN(18)));
     });
 
     it("has expected name", async () => {
         const contract = await POLNToken.deployed();
         const name = await contract.name();
-        assert(name === "fairhive token", `incorrect name, got ${name}, want "fairhive token"`);
+        assert(name === "fairhive token");
     });
 
     it("has expected symbol", async () => {
@@ -28,29 +30,47 @@ contract("POLNToken", (accounts) => {
 
     it("has expected initial supply", async () => {
         const contract = await POLNToken.deployed();
-        const supply = await contract.totalSupply();
-        assert("200000000000000000000000000" === supply.toString(), `incorrect supply, got ${supply.toString()}, want "200000000000000000000000000"`);
-        const bn = web3.utils.toBN(supply);
-        assert(web3.utils.isBN(bn));
-        assert(web3.utils.toBN(web3.utils.toWei("200000000", "ether")).eq(bn));
+        let supply = await contract.totalSupply();
+        assert(supply);
+        assert(supply.toString() === "200000000000000000000000000");
+
+        supply = web3.utils.toBN(supply);
+        assert(web3.utils.isBN(supply));
+        assert(!supply.isZero())
+        assert(supply.eq(totalSupply));
     });
 
     it("has expected initial balance", async () => {
         const contract = await POLNToken.deployed();
-        const supply = await contract.totalSupply();
-        const balance = await contract.balanceOf(accounts[0]);
-        assert(supply.toString() === balance.toString(), `incorrect balance, got ${balance.toString()}, want ${supply.toString()}`);
+        const supply = web3.utils.toBN(await contract.totalSupply());
+        assert(supply);
+        const balance = web3.utils.toBN(await contract.balanceOf(accounts[0]));
+        assert(balance);
+        assert(supply.eq(balance));
     });
 
     it("should transfer", async () => {
-        const contract = await POLNToken.new();
-        const a0b1 = web3.utils.toBN(await contract.balanceOf(accounts[0]));
-        const a1b1 = web3.utils.toBN(await contract.balanceOf(accounts[1]));
-        assert(a1b1.isZero());
-        const amount = 1000;
-        await contract.transfer(accounts[1], amount);
-        const a1b2 = web3.utils.toBN(await contract.balanceOf(accounts[1]));
-        assert(!a1b2.isZero());
-        assert(web3.utils.toBN(amount).eq(a1b2));
+        const contract = await POLNToken.deployed();
+        const account0b1 = web3.utils.toBN(await contract.balanceOf(accounts[0]));
+        assert(account0b1);
+        const account1b1 = web3.utils.toBN(await contract.balanceOf(accounts[1]));
+        assert(account1b1);
+
+        assert(!account0b1.isZero());
+        assert(account0b1.eq(totalSupply));
+        assert(account1b1.isZero());
+
+        const amount = web3.utils.toBN(1000);
+        assert(await contract.transfer(accounts[1], amount.toNumber()));// check return is true
+        const account0b2 = web3.utils.toBN(await contract.balanceOf(accounts[0]));
+        assert(account0b2);
+        const account1b2 = web3.utils.toBN(await contract.balanceOf(accounts[1]));
+        assert(account1b2);
+
+        assert(!account0b2.isZero());
+        assert(account0b2.lte(totalSupply.sub(amount)));
+        assert(!account1b2.isZero());
+        assert(account1b2.eq(amount));
     });
+    //@TODO : test allowance + transferFrom + increase/decreaseAllowance
 });
