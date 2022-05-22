e2c17e04f4ca0d453c1c30e7066f882b521167c9
_whyvrafvr <jsie@trendev.fr>
2022-05-22 13:32:57 +0200

    add more testnets

--- truffle-config.js
+++ truffle-config.js
@@ -18,10 +18,13 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const mnemonic = process.env["MNEMONIC"];
+
+const etherscan_api_key = process.env["ETHERSCAN_API"];
+const snowtrace_api_key = process.env["SNOWTRACE_API"];
+const polygonscan_api_key = process.env["POLYGONSCAN_API"];
+const bscscan_api_key = process.env["BSCSCAN_API"];
 
 module.exports = {
   /**
@@ -41,36 +44,86 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //   host: "127.0.0.1",     // Localhost (default: none)
-    //   port: 7545,            // Standard Ethereum port (default: none)
-    //   network_id: "*",       // Any network (default: none)
-    // },
-    // Another network with more advanced options...
-    // advanced: {
-    // port: 8777,             // Custom port
-    // network_id: 1342,       // Custom network
-    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websocket: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
-    // Useful for private networks
-    // private: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-    // network_id: 2111,   // This network is yours, in the cloud.
-    // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+    rinkeby: {
+      provider: () => {
+        const project_id = process.env["INFURA_PROJECT_ID"]
+
+        return new HDWalletProvider(
+          mnemonic,
+          `https://rinkeby.infura.io/v3/${project_id}`);
+      },
+      network_id: 4
+    },
+
+    kovan: {
+      provider: () => {
+        const project_id = process.env["INFURA_PROJECT_ID"]
+
+        return new HDWalletProvider(
+          mnemonic,
+          `https://kovan.infura.io/v3/${project_id}`);
+      },
+      network_id: 42
+    },
+
+    goerli: {
+      provider: () => {
+        const project_id = process.env["INFURA_PROJECT_ID"]
+
+        return new HDWalletProvider(
+          mnemonic,
+          `https://goerli.infura.io/v3/${project_id}`);
+      },
+      network_id: 5
+    },
+
+    ropsten: {
+      provider: () => {
+        const project_id = process.env["ALCHEMY_PROJECT_ID"]
+
+        return new HDWalletProvider(
+          mnemonic,
+          `https://eth-ropsten.alchemyapi.io/v2/${project_id}`);
+      },
+      network_id: 3
+    },
+
+    // avalanche testnet
+    fuji: {
+      provider: () => {
+        return new HDWalletProvider(
+          mnemonic,
+          `https://api.avax-test.network/ext/bc/C/rpc`);
+      },
+      network_id: 43113,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+
+    bnbt: {
+      provider: () => new HDWalletProvider(
+        mnemonic,
+        `https://data-seed-prebsc-2-s2.binance.org:8545`),
+      network_id: 97,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+
+    mumbai: {
+      provider: () => new HDWalletProvider(
+        mnemonic,
+        `https://rpc-mumbai.matic.today`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
   },
 
   // Set default mocha options here, use special reporters etc.
@@ -93,24 +146,14 @@ module.exports = {
     }
   },
 
-  // Truffle DB is currently disabled by default; to enable it, change enabled:
-  // false to enabled: true. The default storage location can also be
-  // overridden by specifying the adapter settings, as shown in the commented code below.
-  //
-  // NOTE: It is not possible to migrate your contracts to truffle DB and you should
-  // make a backup of your artifacts to a safe location before enabling this feature.
-  //
-  // After you backed up your artifacts you can utilize db by running migrate as follows:
-  // $ truffle migrate --reset --compile-all
-  //
-  // db: {
-  // enabled: false,
-  // host: "127.0.0.1",
-  // adapter: {
-  //   name: "sqlite",
-  //   settings: {
-  //     directory: ".db"
-  //   }
-  // }
-  // }
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+
+  api_keys: {
+    etherscan: etherscan_api_key,
+    snowtrace: snowtrace_api_key,
+    polygonscan: polygonscan_api_key,
+    bscscan: bscscan_api_key
+  }
 };
