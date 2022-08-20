9fc04e13cb958f679d8233fa371e0aa917bf00ab
juliensie <jsie@trendev.fr>
2022-08-20 17:26:36 +0200

    init deployment

--- migrations/4_deploy_waitlist.js
+++ migrations/4_deploy_waitlist.js
@@ -1,6 +1,9 @@
 //WaitList
 const WaitList = artifacts.require("WaitList");
+const Users = artifacts.require("Users");
+const POLNToken = artifacts.require("POLNToken");
+const max = 1000;
 
-module.exports = function(deployer){
-    deployer.deploy(WaitList);
+module.exports = function (deployer) {
+    deployer.deploy(WaitList, max, Users.address, POLNToken.address);
 };
