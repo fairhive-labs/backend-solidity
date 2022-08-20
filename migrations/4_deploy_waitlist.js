cf29d7ae0e309bef87d0115dd580f8f50c1be850
juliensie <jsie@trendev.fr>
2022-08-20 18:19:20 +0200

    transfert max * price poln to waitlist contract

--- migrations/4_deploy_waitlist.js
+++ migrations/4_deploy_waitlist.js
@@ -2,8 +2,12 @@
 const WaitList = artifacts.require("WaitList");
 const Users = artifacts.require("Users");
 const POLNToken = artifacts.require("POLNToken");
-const max = 1000;
+const max = 10000;
+const price = 10;
 
-module.exports = function (deployer) {
-    deployer.deploy(WaitList, max, Users.address, POLNToken.address);
+module.exports = async function (deployer, network, accounts) {
+    await deployer.deploy(WaitList, POLNToken.address, Users.address, max);
+    const waitlist = await WaitList.deployed();
+    const poln = await POLNToken.deployed();
+    await poln.transfer(waitlist.address, max * price);
 };
